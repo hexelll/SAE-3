896954dec43427e6c64e304daaa14635131e7adc
ibarneix <brxiban@gmail.com>
2025-11-20 11:59:09 +0100

    rajout de la creation de la bese de donnée et correction des noms de colonnes pour uniformité

--- BDD/BDD_Projet.sql
+++ BDD/BDD_Projet.sql
@@ -1,47 +1,51 @@
 -- SQL: simple blog schema + sample queries (PostgreSQL-compatible)
 
-BEGIN;
+
+
+CREATE DATABASE BDD_Projet;
+USE BDD_Projet;
 
 CREATE TABLE Materiel (
-    id_Materiel INT AUTO_INCREMENT PRIMARY KEY,
-    nom_materiel VARCHAR(100) NOT NULL,
-    description_materiel TEXT,
-    type_materiel VARCHAR(50),
-    stock_total INT NOT NULL DEFAULT 0,
-    stock_disponible INT NOT NULL DEFAULT 0,
+    idMateriel INT AUTO_INCREMENT PRIMARY KEY,
+    nomMateriel VARCHAR(100) NOT NULL,
+    descriptionMateriel TEXT,
+    typeMateriel VARCHAR(50),
+    stockTotal INT NOT NULL DEFAULT 0,
+    stockDisponible INT NOT NULL DEFAULT 0,
     Empruntable BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE TABLE Fabriquer (
-    id_fabriqué INT,
-    id_composant INT,
-    PRIMARY KEY (id_fabriqué,id_composant),
-    FOREIGN KEY (id_fabriqué) REFERENCES Materiel(id_Materiel),
-    FOREIGN KEY (id_composant) REFERENCES Materiel(id_Materiel)
+    idFabriqué INT,
+    idComposant INT,
+    PRIMARY KEY (idFabriqué,idComposant),
+    FOREIGN KEY (idFabriqué) REFERENCES Materiel(idMateriel),
+    FOREIGN KEY (idComposant) REFERENCES Materiel(idMateriel)
 );
 
 CREATE TABLE Admin (
-    id_admin INT AUTO_INCREMENT PRIMARY KEY,
-    email_admin VARCHAR(100) NOT NULL UNIQUE,
-    hash_admin VARCHAR(255) NOT NULL
+
+    idAdmin INT AUTO_INCREMENT PRIMARY KEY,
+    emailAdmin VARCHAR(100) NOT NULL UNIQUE,
+    hashAdmin VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Emprunt (
-    id_emprunt INT AUTO_INCREMENT PRIMARY KEY,
-    motif_emprunt TEXT,
-    email_emprunt VARCHAR(100) NOT NULL,
-    date_emprunt DATE,
-    date_retour_prevue DATE,
-    date_retour_reelle DATE NULL,
-    statut_emprunt VARCHAR(50) NOT NULL DEFAULT 'en cours',
-    id_admin INT,
-    FOREIGN KEY(id_admin) REFERENCES Admin(id_admin)
+    idEmprunt INT AUTO_INCREMENT PRIMARY KEY,
+    motifEmprunt TEXT,
+    emailEmprunt VARCHAR(100) NOT NULL,
+    dateEmprunt DATE,
+    dateRetourPrevue DATE,
+    dateRetourReelle DATE NULL,
+    statutEmprunt VARCHAR(50) NOT NULL DEFAULT 'en cours',
+    idAdmin INT,
+    FOREIGN KEY(idAdmin) REFERENCES Admin(idAdmin)
 );
 
 CREATE TABLE Emprunter (
-    id_emprunt INT,
-    id_Materiel INT,
-    PRIMARY KEY (id_emprunt, id_Materiel),
-    FOREIGN KEY (id_emprunt) REFERENCES Emprunt(id_emprunt),
-    FOREIGN KEY (id_Materiel) REFERENCES Materiel(id_Materiel)
+    idEmprunt INT,
+    idMateriel INT,
+    PRIMARY KEY (idEmprunt, idMateriel),
+    FOREIGN KEY (idEmprunt) REFERENCES Emprunt(idEmprunt),
+    FOREIGN KEY (idMateriel) REFERENCES Materiel(idMateriel)
 );
